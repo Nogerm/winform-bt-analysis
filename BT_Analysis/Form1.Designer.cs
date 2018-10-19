3406a79382db2344f428fd9b069640938b854635
Andy_CW_Wu <andy_cw_wu@wistron.com>
2018-10-19 19:49:15 +0800

    Initial commit

---
+++ BT_Analysis/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BT_Analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.deviceListBox = new System.Windows.Forms.ListBox();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.pbSearch = new System.Windows.Forms.ProgressBar();
+            this.tim_query = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // deviceListBox
+            // 
+            this.deviceListBox.FormattingEnabled = true;
+            this.deviceListBox.ItemHeight = 12;
+            this.deviceListBox.Location = new System.Drawing.Point(12, 12);
+            this.deviceListBox.Name = "deviceListBox";
+            this.deviceListBox.Size = new System.Drawing.Size(172, 112);
+            this.deviceListBox.TabIndex = 0;
+            this.deviceListBox.SelectedIndexChanged += new System.EventHandler(this.deviceListBox_SelectedIndexChanged);
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(200, 12);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(75, 41);
+            this.btn_connect.TabIndex = 1;
+            this.btn_connect.Text = "Connect";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(292, 12);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(75, 41);
+            this.btn_disconnect.TabIndex = 2;
+            this.btn_disconnect.Text = "Disconnect";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            this.btn_disconnect.Click += new System.EventHandler(this.btn_disconnect_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(198, 69);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(33, 12);
+            this.lbl_status.TabIndex = 3;
+            this.lbl_status.Text = "label1";
+            // 
+            // pbSearch
+            // 
+            this.pbSearch.Location = new System.Drawing.Point(23, 59);
+            this.pbSearch.Name = "pbSearch";
+            this.pbSearch.Size = new System.Drawing.Size(150, 22);
+            this.pbSearch.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pbSearch.TabIndex = 4;
+            // 
+            // tim_query
+            // 
+            this.tim_query.Interval = 200;
+            this.tim_query.Tick += new System.EventHandler(this.tim_query_Tick);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(12, 130);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(822, 389);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(846, 531);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pbSearch);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_connect);
+            this.Controls.Add(this.deviceListBox);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox deviceListBox;
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_disconnect;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.ProgressBar pbSearch;
+        private System.Windows.Forms.Timer tim_query;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ BT_Analysis/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.Net.Sockets;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace BT_Analysis
+{
+    public partial class Form1 : Form
+    {
+        private BluetoothClient bluetoothClient;
+        private BackgroundWorker bg_worker;
+        private BluetoothAddress selectedAddr;
+        private NetworkStream stream = null;
+
+        private List<String> items_name = new List<String>();
+        private List<BluetoothAddress> items_addr = new List<BluetoothAddress>();
+        private String selectedName;
+        private Queue<double> dataQueue = new Queue<double>(1024);
+        private int new_point_cnt = 0;
+
+        byte[] raw_buffer = new byte[1024];
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitChart();
+
+            lbl_status.Text = "Scaning ...";
+
+            bg_worker = new BackgroundWorker();
+            bg_worker.DoWork += new DoWorkEventHandler(bg_DoWork);
+            bg_worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_RunWorkerCompleted);
+
+            bg_worker.RunWorkerAsync();
+        }
+
+        void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            string[] items = e.Result as string[];
+            foreach (string item in items)
+            {
+                deviceListBox.Items.Add(item);
+            }
+            pbSearch.Hide();
+            lbl_status.Text = "Scan complete";
+        }
+
+        void bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BluetoothClient scanClient = new BluetoothClient();
+            BluetoothDeviceInfo[] devices = scanClient.DiscoverDevices();
+            foreach (BluetoothDeviceInfo device in devices)
+            {
+                items_name.Add(device.DeviceName);
+                items_addr.Add(device.DeviceAddress);
+            }
+            e.Result = items_name.ToArray();
+        }
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            lbl_status.Text = "";
+            try
+            {
+                bluetoothClient = new BluetoothClient();
+                BluetoothEndPoint localEndpoint = new BluetoothEndPoint(selectedAddr, BluetoothService.SerialPort);
+                bluetoothClient.Connect(localEndpoint);
+                if (bluetoothClient.Connected)
+                {
+                    lbl_status.Text = selectedName + " Connected";
+
+                    stream = bluetoothClient.GetStream();
+                    tim_query.Start();
+                }
+            }
+            catch (System.Net.Sockets.SocketException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void btn_disconnect_Click(object sender, EventArgs e)
+        {
+            if (bluetoothClient != null)
+            {
+                bluetoothClient.Close();
+                bluetoothClient = null;
+                tim_query.Stop();
+                lbl_status.Text = "Disconnected";
+            }
+        }
+
+        private void deviceListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedAddr = items_addr[deviceListBox.SelectedIndex];
+            selectedName = items_name[deviceListBox.SelectedIndex];
+            lbl_status.Text = "Selected Address: " + selectedAddr.ToString();
+        }
+
+        private void tim_query_Tick(object sender, EventArgs e)
+        {
+            //query from BT and add to queue
+            new_point_cnt = 5;
+
+            //Dequeue if too many data
+            if (dataQueue.Count > 1024)
+            {
+                for (int i = 0; i < new_point_cnt; i++)
+                {
+                    dataQueue.Dequeue();
+                }
+            }
+
+            //Enqueue new data
+            Random r = new Random();
+            for (int i = 0; i < new_point_cnt; i++)
+            {
+                dataQueue.Enqueue(r.Next(0, 100));
+            }
+
+            //Add point to chart
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+            }
+        }
+
+        private void InitChart()
+        {
+            //Define chart area
+            this.chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("C1");
+            this.chart1.ChartAreas.Add(chartArea1);
+            //Define chart series
+            this.chart1.Series.Clear();
+            Series series1 = new Series("S1");
+            series1.ChartArea = "C1";
+            this.chart1.Series.Add(series1);
+            //Set chartArea Apperence
+            this.chart1.ChartAreas[0].AxisX.Maximum = 1024;
+            this.chart1.ChartAreas[0].AxisX.Interval = 50;
+            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
+            this.chart1.ChartAreas[0].AxisY.Maximum = 100;
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            //Set chart title
+            this.chart1.Titles.Clear();
+            this.chart1.Titles.Add("S01");
+            this.chart1.Titles[0].Text = "Pitch";
+            this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            //Set Chart Apperence
+            this.chart1.Series[0].Color = Color.Red;
+            this.chart1.Series[0].ChartType = SeriesChartType.Line;
+            this.chart1.Series[0].Points.Clear();
+        }
+    }
+}
