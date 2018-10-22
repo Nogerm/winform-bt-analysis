7df3a19a33a1cff9d735c267c0bdb627d9f23ce5
Andy_CW_Wu <andy_cw_wu@wistron.com>
2018-10-22 18:32:14 +0800

    Receive stream and display

--- BT_Analysis/Form1.Designer.cs
+++ BT_Analysis/Form1.Designer.cs
@@ -29,9 +29,15 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.deviceListBox = new System.Windows.Forms.ListBox();
             this.btn_connect = new System.Windows.Forms.Button();
             this.btn_disconnect = new System.Windows.Forms.Button();
@@ -39,7 +45,11 @@
             this.pbSearch = new System.Windows.Forms.ProgressBar();
             this.tim_query = new System.Windows.Forms.Timer(this.components);
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
             this.SuspendLayout();
             // 
             // deviceListBox
@@ -96,24 +106,58 @@
             // 
             // chart1
             // 
-            chartArea4.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea4);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
             this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            legend4.Name = "Legend1";
-            this.chart1.Legends.Add(legend4);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(12, 130);
             this.chart1.Name = "chart1";
-            series4.ChartArea = "ChartArea1";
-            series4.Legend = "Legend1";
-            series4.Name = "Series1";
-            this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(822, 389);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(984, 193);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 345);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(485, 305);
+            this.chart2.TabIndex = 6;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(511, 345);
+            this.chart3.Name = "chart3";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart3.Series.Add(series3);
+            this.chart3.Size = new System.Drawing.Size(485, 305);
+            this.chart3.TabIndex = 7;
+            this.chart3.Text = "chart3";
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(846, 531);
+            this.ClientSize = new System.Drawing.Size(1008, 660);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.pbSearch);
             this.Controls.Add(this.lbl_status);
@@ -122,6 +166,8 @@
             this.Controls.Add(this.deviceListBox);
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -136,6 +182,8 @@
         private System.Windows.Forms.ProgressBar pbSearch;
         private System.Windows.Forms.Timer tim_query;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
     }
 }
 
--- BT_Analysis/Form1.cs
+++ BT_Analysis/Form1.cs
@@ -13,34 +13,65 @@ using InTheHand.Net.Sockets;
 using System.Net.Sockets;
 using System.Windows.Forms.DataVisualization.Charting;
 
+struct Motor_state {
+    public float voltage;
+    public float current;
+    public int rpm;
+}
+
+struct Package {
+    public float pitch;
+    public Motor_state left_motor;
+    public Motor_state right_motor;
+}
+
 namespace BT_Analysis
 {
     public partial class Form1 : Form
     {
+        //Const
+        private const int PACKAGE_QUERY  = 0xFF;
+        private const int PACKAGE_HEADER = 0xFE;
+        private const int PACKAGE_SPLIT  = 0x2C;
+        private const int PACKAGE_END    = 0x0D;
+
+        private const int SAMPLES_PER_CHART = 1024;
+        private const int BUFFER_PER_PACKAGE = 1024;
+        private const int AXIS_X_INTERVAL = 50;
+
+        private const int PITCH_MAX = 90;
+        private const int PITCH_MIN = -90;
+        private const int VOLTAGE_MAX = 36;
+        private const int VOLTAGE_MIN = -5;
+        private const int CURRENT_MAX = 5;
+        private const int CURRENT_MIN = -1;
+        private const int RPM_MAX = 3000;
+        private const int RPM_MIN = -3000;
+
+        //Bluetooth connect
         private BluetoothClient bluetoothClient;
         private BackgroundWorker bg_worker;
-        private BluetoothAddress selectedAddr;
-        private NetworkStream stream = null;
-
         private List<String> items_name = new List<String>();
         private List<BluetoothAddress> items_addr = new List<BluetoothAddress>();
         private String selectedName;
-        private Queue<double> dataQueue = new Queue<double>(1024);
-        private int new_point_cnt = 0;
+        private BluetoothAddress selectedAddr;
+        private NetworkStream stream = null;
 
-        byte[] raw_buffer = new byte[1024];
+        //Data decode
+        private Queue<double> rawQueue = new Queue<double>(BUFFER_PER_PACKAGE);
+        private Queue<Package> dataQueue = new Queue<Package>(SAMPLES_PER_CHART);
+        private int new_buffer_cnt = 0;
 
         public Form1()
         {
             InitializeComponent();
             InitChart();
 
+            //Scan device in background
             lbl_status.Text = "Scaning ...";
-
             bg_worker = new BackgroundWorker();
             bg_worker.DoWork += new DoWorkEventHandler(bg_DoWork);
             bg_worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bg_RunWorkerCompleted);
-
             bg_worker.RunWorkerAsync();
         }
 
@@ -79,8 +110,19 @@ namespace BT_Analysis
                 {
                     lbl_status.Text = selectedName + " Connected";
 
+                    //start receive data
                     stream = bluetoothClient.GetStream();
+                    rawQueue.Clear();
+                    dataQueue.Clear();
                     tim_query.Start();
+                    Task.Factory.StartNew(() =>
+                    {
+                        while (bluetoothClient.Connected) {
+                            if (stream.CanRead) {
+                                rawQueue.Enqueue(stream.ReadByte());
+                            }
+                        }
+                    });
                 }
             }
             catch (System.Net.Sockets.SocketException err)
@@ -96,6 +138,8 @@ namespace BT_Analysis
                 bluetoothClient.Close();
                 bluetoothClient = null;
                 tim_query.Stop();
+                dataQueue.Clear();
+                tim_query.Start();
                 lbl_status.Text = "Disconnected";
             }
         }
@@ -109,30 +153,81 @@ namespace BT_Analysis
 
         private void tim_query_Tick(object sender, EventArgs e)
         {
-            //query from BT and add to queue
-            new_point_cnt = 5;
+            //Request data
+            stream.WriteByte(PACKAGE_QUERY);
 
-            //Dequeue if too many data
-            if (dataQueue.Count > 1024)
-            {
-                for (int i = 0; i < new_point_cnt; i++)
+            //raw data count
+            new_buffer_cnt = rawQueue.Count;
+
+            if (new_buffer_cnt > 3) {
+                //decode new data
+                byte incoming_byte;
+                bool should_record = false;
+                byte[] raw_byte = new byte[64];
+                int raw_byte_ptr = 0;
+
+                for (int i = 0; i < new_buffer_cnt; i++)
                 {
-                    dataQueue.Dequeue();
+                    incoming_byte = (byte)rawQueue.Dequeue();
+                    if (should_record) {
+                        if (incoming_byte == PACKAGE_END)
+                        {
+                            //complete one new data
+                            should_record = false;
+                            string new_data_str = System.Text.Encoding.UTF8.GetString(raw_byte, 0, raw_byte_ptr);
+                            string[] new_datas = new_data_str.Split(',');
+                            Package new_package = new Package();
+
+                            new_package.pitch               = float.Parse(new_datas[0], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.left_motor.voltage  = float.Parse(new_datas[1], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.left_motor.current  = float.Parse(new_datas[2], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.left_motor.rpm      = int.Parse(  new_datas[3], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.right_motor.voltage = float.Parse(new_datas[4], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.right_motor.current = float.Parse(new_datas[5], System.Globalization.CultureInfo.InvariantCulture);
+                            new_package.right_motor.rpm     = int.Parse(  new_datas[6], System.Globalization.CultureInfo.InvariantCulture);
+
+                            //Dequeue if too many data
+                            if (dataQueue.Count > 1024)
+                            {
+                                dataQueue.Dequeue();
+                            }
+
+                            //Enqueue new data
+                            dataQueue.Enqueue(new_package);
+
+                            //reset buffer pointer
+                            raw_byte_ptr = 0;
+
+                            //Add point to chart
+                            this.chart1.Series[0].Points.Clear();
+                            this.chart2.Series[0].Points.Clear();
+                            this.chart2.Series[1].Points.Clear();
+                            this.chart2.Series[2].Points.Clear();
+                            this.chart3.Series[0].Points.Clear();
+                            this.chart3.Series[1].Points.Clear();
+                            this.chart3.Series[2].Points.Clear();
+                            for (int idx = 0; idx < dataQueue.Count; idx++)
+                            {
+                                this.chart1.Series[0].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).pitch);
+                                this.chart2.Series[0].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).left_motor.voltage);
+                                this.chart2.Series[1].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).left_motor.current);
+                                this.chart2.Series[2].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).left_motor.rpm);
+                                this.chart3.Series[0].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).right_motor.voltage);
+                                this.chart3.Series[1].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).right_motor.current);
+                                this.chart3.Series[2].Points.AddXY((idx + 1), dataQueue.ElementAt(idx).right_motor.rpm);
+                            }
+                        }
+                        else {
+                            //receiving byte
+                            raw_byte[raw_byte_ptr] = incoming_byte;
+                            raw_byte_ptr++;
+                        }
+                    }
+                    if (incoming_byte == PACKAGE_HEADER)
+                    {
+                        should_record = true;
+                    }
                 }
-            }
-
-            //Enqueue new data
-            Random r = new Random();
-            for (int i = 0; i < new_point_cnt; i++)
-            {
-                dataQueue.Enqueue(r.Next(0, 100));
-            }
-
-            //Add point to chart
-            this.chart1.Series[0].Points.Clear();
-            for (int i = 0; i < dataQueue.Count; i++)
-            {
-                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
             }
         }
 
@@ -142,28 +237,110 @@ namespace BT_Analysis
             this.chart1.ChartAreas.Clear();
             ChartArea chartArea1 = new ChartArea("C1");
             this.chart1.ChartAreas.Add(chartArea1);
+            this.chart2.ChartAreas.Clear();
+            ChartArea chartArea2 = new ChartArea("C2");
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart3.ChartAreas.Clear();
+            ChartArea chartArea3 = new ChartArea("C3");
+            this.chart3.ChartAreas.Add(chartArea3);
+            
             //Define chart series
             this.chart1.Series.Clear();
-            Series series1 = new Series("S1");
+            Series series1 = new Series("Pitch");
             series1.ChartArea = "C1";
             this.chart1.Series.Add(series1);
+
+            this.chart2.Series.Clear();
+            Series series2 = new Series("Voltage_L");
+            series2.ChartArea = "C2";
+            this.chart2.Series.Add(series2);
+            Series series3 = new Series("Current_L");
+            series3.ChartArea = "C2";
+            this.chart2.Series.Add(series3);
+            Series series4 = new Series("RPM_L");
+            series4.ChartArea = "C2";
+            this.chart2.Series.Add(series4);
+
+            this.chart3.Series.Clear();
+            Series series5 = new Series("Voltage_R");
+            series5.ChartArea = "C3";
+            this.chart3.Series.Add(series5);
+            Series series6 = new Series("Current_R");
+            series6.ChartArea = "C3";
+            this.chart3.Series.Add(series6);
+            Series series7 = new Series("RPM_R");
+            series7.ChartArea = "C3";
+            this.chart3.Series.Add(series7);
+
             //Set chartArea Apperence
-            this.chart1.ChartAreas[0].AxisX.Maximum = 1024;
-            this.chart1.ChartAreas[0].AxisX.Interval = 50;
-            this.chart1.ChartAreas[0].AxisY.Minimum = 0;
-            this.chart1.ChartAreas[0].AxisY.Maximum = 100;
+            this.chart1.ChartAreas[0].AxisX.Interval = AXIS_X_INTERVAL;
+            this.chart1.ChartAreas[0].AxisX.Maximum = SAMPLES_PER_CHART;
+            this.chart1.ChartAreas[0].AxisY.Minimum = PITCH_MIN;
+            this.chart1.ChartAreas[0].AxisY.Maximum = PITCH_MAX;
             this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
             this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+
+            this.chart2.ChartAreas[0].AxisX.Interval = AXIS_X_INTERVAL;
+            this.chart2.ChartAreas[0].AxisX.Maximum = SAMPLES_PER_CHART/2;
+            this.chart2.ChartAreas[0].AxisY.Minimum = VOLTAGE_MIN;
+            this.chart2.ChartAreas[0].AxisY.Maximum = VOLTAGE_MAX;
+            this.chart2.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart2.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+
+            this.chart3.ChartAreas[0].AxisX.Interval = AXIS_X_INTERVAL;
+            this.chart3.ChartAreas[0].AxisX.Maximum = SAMPLES_PER_CHART/2;
+            this.chart3.ChartAreas[0].AxisY.Minimum = VOLTAGE_MIN;
+            this.chart3.ChartAreas[0].AxisY.Maximum = VOLTAGE_MAX;
+            this.chart3.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart3.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+
             //Set chart title
             this.chart1.Titles.Clear();
-            this.chart1.Titles.Add("S01");
+            this.chart1.Titles.Add("T01");
             this.chart1.Titles[0].Text = "Pitch";
             this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
             this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+
+            this.chart2.Titles.Clear();
+            this.chart2.Titles.Add("T02");
+            this.chart2.Titles[0].Text = "Motor Left";
+            this.chart2.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart2.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+
+            this.chart3.Titles.Clear();
+            this.chart3.Titles.Add("T03");
+            this.chart3.Titles[0].Text = "Motor Right";
+            this.chart3.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart3.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+
             //Set Chart Apperence
             this.chart1.Series[0].Color = Color.Red;
             this.chart1.Series[0].ChartType = SeriesChartType.Line;
             this.chart1.Series[0].Points.Clear();
+
+            this.chart2.Series[0].Color = Color.Red;
+            this.chart2.Series[0].ChartType = SeriesChartType.Line;
+            this.chart2.Series[0].Points.Clear();
+
+            this.chart2.Series[1].Color = Color.Blue;
+            this.chart2.Series[1].ChartType = SeriesChartType.Line;
+            this.chart2.Series[1].Points.Clear();
+
+            this.chart2.Series[2].Color = Color.Green;
+            this.chart2.Series[2].ChartType = SeriesChartType.Line;
+            this.chart2.Series[2].Points.Clear();
+
+            this.chart3.Series[0].Color = Color.Red;
+            this.chart3.Series[0].ChartType = SeriesChartType.Line;
+            this.chart3.Series[0].Points.Clear();
+
+            this.chart3.Series[1].Color = Color.Blue;
+            this.chart3.Series[1].ChartType = SeriesChartType.Line;
+            this.chart3.Series[1].Points.Clear();
+
+            this.chart3.Series[2].Color = Color.Green;
+            this.chart3.Series[2].ChartType = SeriesChartType.Line;
+            this.chart3.Series[2].Points.Clear();
         }
     }
 }
